3e91a1d6f7a7feaa0717555bbb5aec3817c1a0e2
GUILHERME SILVA DE OLIVEIRA <guilherme.oliveira163@fatec.sp.gov.br>
2020-09-08 10:50:57 -0300

    Faz soma e subtracao

--- aula.py
+++ aula.py
@@ -1,5 +1,11 @@
-print('SOMA')
+print('SOMA ou SUBTRAÇÃO')
+escolha = int(input("1-soma 2-subtração"))
 a = int (input('Digite um valor númerico inteiro'))
 b = int (input('Digite um valor númerico inteiro'))
-result = a + b
+result = 0
+if op ==1:
+ result = a+b
+else:
+ result = a-b
+
 print(result)
